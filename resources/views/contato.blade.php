dad2277019951e4721b969db4852613ea90153a6
Bii <105325395+Biielz@users.noreply.github.com>
2023-10-24 08:43:27 -0300

    Add files via upload

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/estilos.css">
+</head>
+<body>
+    <div id="container">
+        <h3>Página de Contato</h3>
+        <div class="content">
+            <a href="/">Voltar para home</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="/css/estilos.css">
+</head>
+<body>
+    <div id="container">
+        <h3>Home Page</h3>
+        <nav class="nav">
+            <ul>
+                <li><a href="produtos">Produtos</a></li>
+                <li><a href="contato">Contato</a></li>
+            </ul>
+        </nav>
+        <div class="content">
+            <img src="/images/home.png" alt="wawa" width="1000rem">
+        </div>
+    </div>
+</body>
+</html>
